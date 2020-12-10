da5bcd1e68876fc22bff0e0d7c4305571abd8c2c
Francisco Arredondo <frarredo@gmail.com>
2020-12-10 10:46:40 -0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import { createToken, CstParser, Lexer } from 'chevrotain';
+import fs from 'fs';
+import path from 'path';
+
+const division = createToken({ name: 'TARGET', pattern: '/TARGET/' });
+
+const allTokens = [division];
+
+const PowerOnLexer = new Lexer(allTokens);
+
+const filePath = path.join(
+  __dirname,
+  '..',
+  'sample_files',
+  'ACCT.WIRE.EMAIL.SYMC.PO'
+);
+const stream = fs.createReadStream(path.normalize(filePath));
+stream.setEncoding('utf8');
+
+stream.addListener('data', chunk => {
+  // console.log(chunk.toString());
+  // console.log(chunk);
+  const tokens = PowerOnLexer.tokenize(chunk as string);
+  console.log(tokens);
+});
+
+// class PowerOnParser extends CstParser {
+//   constructor() {
+//     super(allTokens);
+//     this.performSelfAnalysis();
+//   }
+// }
